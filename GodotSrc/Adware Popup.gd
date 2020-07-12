feca39edcff11b71d32ce1fd026089867554047e
Zanark <zanarkdev@gmail.com>
2020-07-12 17:56:00 +0530

    Random Pop-Ups working

--- GodotSrc/Adware Popup.gd
+++ GodotSrc/Adware Popup.gd
@@ -5,9 +5,16 @@ onready var Children = get_children()
 func _ready():
 	print(get_child_count())
 	print(get_children())
-	random_popup()
+	filter_Children()
+	
+func filter_Children():
+	for i in Children:
+		if i.get_name() == "Timer":
+			Children.erase(i)
 	
 func random_popup():
-	for i in Children:
-		i.popup()
+	var child = Children[randi() % Children.size()]
+	child.popup()
 
+func _on_Timer_timeout():
+	random_popup()
